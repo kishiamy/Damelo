22dc9ab26178a532cb5cd39d5636d4954363bf14
bita <kishiamy84@gmail.com>
2011-12-02 21:14:57 +0000

    idem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Damelo::Application.config.secret_token = '64833ad784a2193dd73e400b682dd5804b1e95f68106f2572f138526a08ef1c3ba4c9d00442ae154099629b72add58d7552ff179837dfe5385e9f26a0a28a6e5'
